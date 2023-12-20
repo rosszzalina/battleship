7e8cd01c06e44a1285bf7e1a2c7ca9dbdce65747
rosszzalina <150505816+rosszzalina@users.noreply.github.com>
2023-12-20 15:06:47 +0600

    battleship.cpp

--- battleship.cpp
+++ battleship.cpp
@@ -1,58 +1,92 @@
 #include <iostream>
-#include <algorithm>
+#include <cstdlib>
 #include <ctime>
 using namespace std;
 
-int main() {
-    const int n = 10;
-    int size;
-    int arr[n] = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
-    cout << "Welcome to the Memory Game!";
-    cout << endl;
-    cout << "Enter the size of an array: ";
-    cin >> size;
-    if (size != n) {
-        cout << "Please enter the correct size: " << n << endl;
-        return 1;
-    }
-    int found[n] = {0}; // Array to keep track of found elements
-    int cnt = 0, moves = 0;
-    srand(time(0)); // Seed for random number generation
-    random_shuffle(arr, arr + n); // Shuffle the array
+const int boardSize = 7;
 
-    while (cnt < n) {
-        // Display the array elements (either '?' or the numbers found)
-        for (int i = 0; i < n; i++) {
-            if (found[i] == 0) {
-                cout << "? ";
-            } else {
-                cout << arr[i] << " ";
-            }
+struct Ship {
+    int length;
+    int hits;
+};
+
+struct Player {
+    string name;
+    int shots;
+};
+
+char board[boardSize][boardSize];
+
+int initializeBoard() {
+    for (int i = 0; i < boardSize; ++i) {
+        for (int j = 0; j < boardSize; ++j) {
+            board[i][j] = ' ';
+        }
+    }
+    board[1][1] = board[1][2] = board[1][3] = 'S';  
+    board[3][3] = board[3][4] = 'S';                
+    board[5][5] = 'S';                              
+}
+
+int printBoard() {
+    for (int i = 0; i < boardSize; ++i) {
+        for (int j = 0; j < boardSize; ++j) {
+            cout << board[i][j] << ' ';
         }
         cout << endl;
-        int a, b;
-        cout << "Enter two indices: ";
-        cin >> a >> b;
-        if (a < 0 || a >= n || b < 0 || b >= n || found[a] == 1 || found[b] == 1 || a == b) {
-            cout << "Invalid indices, please try again." << endl;
-            continue;
-        }
-        moves++;
-        cout << "The number at index " << a << " is " << arr[a] << endl;
-        cout << "The number at index " << b << " is " << arr[b] << endl;
-        if (arr[a] == arr[b]) {
-            cout << "These numbers are equal!" << endl;
-            found[a] = 1;
-            found[b] = 1;
-            cnt += 2; // Increment by 2 as we found a pair
-        } else {
-            cout << "These numbers are not equal." << endl;
-        }
-        system("cls"); // Use "clear" for Unix-based systems (Linux or macOS)
-        if (cnt == n) {
-            cout << "Congratulations! You found all pairs in " << moves << " moves." << endl;
-            break;
+    }
+}
+
+bool isValidMove(int row, int col) {
+    return row >= 0 && row < boardSize && col >= 0 && col < boardSize && board[row][col] == ' ';
+}
+
+bool isGameOver() {
+    for (int i = 0; i < boardSize; ++i) {
+        for (int j = 0; j < boardSize; ++j) {
+            if (board[i][j] == 'S') {
+                return false; 
+            }
         }
     }
+    return true; 
+}
+
+int playGame(const string& playerName) {
+    initializeBoard();
+    int shots = 0;
+
+    while (!isGameOver()) {
+        int row, col;
+    cout << playerName << ", enter your guess (row and column): ";
+    cin >> row >> col;
+
+        if (!isValidMove(row, col)) {
+            cout << "Invalid move. Try again." << endl;
+            continue;
+        }
+
+        if (board[row][col] == 'S') {
+            cout << "Hit!" << endl;
+            board[row][col] = 'X';  
+        } else {
+            cout << "Miss!" << endl;
+            board[row][col] = 'O';  
+        }
+        printBoard();
+        shots++;
+    }
+
+    cout << "Congratulations, " << playerName << "! You sank all the ships in " << shots << " shots." << endl;
+}
+
+int main() {
+    srand((time(0)));
+    string playerName;
+    cout << "Enter your name: ";
+    cin >> playerName;
+
+    playGame(playerName);
+
     return 0;
 }
